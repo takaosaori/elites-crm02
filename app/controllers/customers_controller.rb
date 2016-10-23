3cd5a838829846d8b17aa58f954a3991d03d2a24
takao saori <takao-saori@av.avex.co.jp>
2016-10-23 07:12:42 +0000

    customer のCRUD周りを開発_1

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,11 +1,17 @@
 class CustomersController < ApplicationController
+  
   def index
   end
 
   def new
+    @customer = Customer.new
+    
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    @customer.save
+    redirect_to @customer
   end
 
   def edit
@@ -15,8 +21,16 @@ class CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def destroy
   end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:family_name, :given_name, :email)
+  end
+  
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ActiveRecord::Base
+    
+    def full_name
+      family_name + given_name
+    end
+
 end
